00d3cf7c764f23fa90cd18c4e857a4ff5dc50560
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-12-13 18:12:06 +0100

    log

--- get_next_version.sh
+++ get_next_version.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 VERSION=0;
-current_branch=$(git rev-parse --abbrev-ref HEAD)
+current_branch="v33"
 all_branches=$(git ls-remote --heads origin  | sed 's?.*refs/heads/??' )
 tags=$(git ls-remote --tags origin | sed 's?.*refs/tags/??' )
+echo "$tags"
+
 all_major_versions=$(echo "$all_branches" | tr ' ' '\n' | grep -o -E '^v.*' | grep -o -E '[0-9]+')
 
+echo "$all_major_versions"
 if [[ "$current_branch" == "master" ]]; then 
   last_major_version=$(echo "${all_major_versions[*]}" | sort -nr | head -n1 )
   next_major_version=$(($last_major_version + 1))
@@ -13,16 +16,20 @@ if [[ "$current_branch" == "master" ]]; then
   VERSION=$next_major_version
 else 
   current_major_version=$(echo "${current_branch}" | sed 's/v//g')
+  echo "$current_major_version"
   patches=($(echo "${tags[*]}" |
      tr ' ' '\n' | 
      grep -o -E "^2.${current_major_version}.\d" ))
   
+  echo "$patches"
   last_patch=$(echo "${patches[*]}" | tr ' ' '\n' | sort -nr | head -n1 )
-
+  echo "$last_patch"
   next_patch=$(($(echo $last_patch | 
     sed "s/2.${current_major_version}.//") + 1))
     
+  echo "$next_patch"
   next_patch=$(echo $next_patch | sed "s/^/2.${current_major_version}./")
+  echo "$next_patch"
   VERSION=$next_patch
 fi
 
