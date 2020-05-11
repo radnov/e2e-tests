b2fdd5d19096b4ce5ff78421de0ed02a2968a219
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2020-05-11 18:14:40 +0200

    fix

--- support/wait/waitForWindowToLoad.js
+++ support/wait/waitForWindowToLoad.js
@@ -5,7 +5,7 @@ module.exports = () => {
   let maxRetries = 25;
   
   const pageSource = () => {
-    return browser.$('html').getHTML()
+    return browser.$('body').getHTML()
   }
 
   let source = pageSource();
